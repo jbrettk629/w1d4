5da83024d1612f6ab6e7486df72bad3d95b3f43b
Ryan Alejandro <alejandr257@gmail.com>
2018-08-23 14:40:35 -0700

    fix issues with getting and parsing pos & value

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 require_relative "tile"
+require 'byebug'
 
 class Board
   def self.empty_grid
@@ -28,7 +29,7 @@ class Board
 
   def []=(pos, value)
     x, y = pos
-    tile = grid[x][y]
+    tile = @grid[x][y]
     tile.value = value
   end
 
